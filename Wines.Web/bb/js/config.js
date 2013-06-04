120f30dc01d113b1415f4b850f9387b1a105c6ff
Sergio Guillen <sergio.guillen@tierconnect.com>
2013-06-04 09:21:18 -0400

    test

--- Wines.Web/bb/js/config.js
+++ Wines.Web/bb/js/config.js
@@ -1,7 +1,8 @@
 window.CellarConfig = {
-  status: 'prod', // debug|prod
+  status: 'remote', // debug|prod|remote
   debug: 'http://localhost:50056',
   prod: 'http://localhost/wines',
+  remote: 'http://10.100.1.183/wines',
   getEndPoint: function() {
     return this[this.status];
   }
